e5cfc57792de8b8bbe0c3fc31d45e2a61cb8d728
zengguoyang <qbjooo@qq.com>
2021-06-18 10:40:18 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sphinx_thunlp_theme',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/a710128/sphinx_thunlp_docs',
     license='MIT',
     author='a710128',
